b912cb8609cb09635a1ce08a07fa48b40d718945
DanielZ1337 <danielz2nd@hotmail.com>
2023-09-12 15:50:17 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,10 @@ import type { NextRequest } from 'next/server'
 
 // the following code is taken from : https://nextjs.org/docs/advanced-features/middleware#setting-headers
 export function middleware(request: NextRequest) {
-    // Clone the request headers and set a new header `x-hello-from-middleware1`
+    // Clone the request headers and set a new header `x-pathname`
     const requestHeaders = new Headers(request.headers)
     requestHeaders.set('x-pathname', request.nextUrl.pathname)
-   
-    // You can also set request headers in NextResponse.rewrite
+    
     return NextResponse.next({
       request: {
         // New request headers
